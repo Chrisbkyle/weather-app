1567001c851be3396b911c7f28973557b19cfb5b
Chris <christopher.b.kyle@gmail.com>
2023-08-09 23:44:15 +1200

    added footer, next add testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <div>
             <div className='footer'>
-                <div className='footerText'>Made using <a href='www.openweathermap.org'>www.openweathermap.org</a>'s One Call API 3.0</div>
+                <div className='footerText'>Made using <a href='https://www.openweathermap.org' target="_blank">www.openweathermap.org</a>'s One Call API 3.0</div>
             </div>
         </div>
     )
