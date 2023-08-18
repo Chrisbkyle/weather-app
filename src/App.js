6f48b85935347026eb176be6bc72866512c79275
Chris <christopher.b.kyle@gmail.com>
2023-08-18 21:44:06 +1200

    background color change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign: 'center', color: 'white'}}>Auckland Weather</h1>
+      <h1 style={{textAlign: 'center'}}>Auckland Weather</h1>
       <GetApiClass />
       <Footer />
     </div>
