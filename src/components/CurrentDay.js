a86addf2f2f04a74d9d8c8a23f321add979ac2d9
Chris <christopher.b.kyle@gmail.com>
2024-04-11 01:30:21 +1200

    direction arrow

--- src/components/CurrentDay.js
+++ src/components/CurrentDay.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React , {useState} from "react";
 import { toUpper, windDir } from './Utils';
 
+const utcToHours = (utc) => {
+    let time = new Date(utc);
+    let hour = time.getHours();
+    let minutes = time.getMinutes();
+    console.log(time)
+    return hour +':'+ minutes   
+}
 
 export default function CurrentDay(props) {
 
-    // console.log(props.data)
+    console.log(props.data)
+
 
     
     let today = new Date()
@@ -13,23 +21,27 @@ export default function CurrentDay(props) {
     return (
         <div>
             <div className='currentContainer'>
-                <div className='currentWeather flex-col'>
-                    <figure className='flex-row'>
+                <div className='currentWeather flex-row'>
+                    <figure>
                         <img src={`https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`} alt='icon of the current weather'/>
                         <figcaption>{toUpper(props.data.weather[0].description)}</figcaption>
                     </figure>
                     <ul className='currentTempHum'>
-                        <li>Temperature {props.data.temp} {'\u2103'}</li>
-                        <li>Humidity {props.data.humidity} %</li>
-                        <li>Feels Like {props.data.feelsLike} {'\u2103'}</li>
+                        <li>Temperature {props.data.temp}{'\u2103'}</li>
+                        <li>Humidity {props.data.humidity}%</li>
+                        <li>Feels Like {props.data.feels_like}{'\u2103'}</li>
+                        <br></br>
+                        <li>Sunrise {utcToHours(props.data.sunrise) + 'AM'}</li>
+                        <li>Sunset {utcToHours(props.data.sunset) + 'PM'}</li>
                     </ul>
                 </div>
                 <div className='currentTimeTemp flex-row'>
                     <div className='currentTime'>{today.toLocaleDateString('en-US', formatOptions)}</div>
                 </div>
                 <div className='currentWind flex-row'>
-                    <div>Wind Speed {props.data.windSpeed} kph</div>
-                    <div>Direction {windDir(props.data.windDirection)}</div>    
+                    <div>Wind Speed {props.data.wind_speed} kph</div>
+                    <img src={'./resources/Dircetion/N.png'}></img>
+                    <div>Direction {windDir(props.data.wind_deg)}</div>    
                 </div>
             </div>
         </div>
