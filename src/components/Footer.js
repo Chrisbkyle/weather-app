a003bc9d07435b3103a17cc74de65c278857d8ee
Chris <christopher.b.kyle@gmail.com>
2024-04-12 15:24:24 +1200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return(
         <div>
             <div className='footer'>
-                <div className='footerText'>Made using <a href='https://www.openweathermap.org' target="_blank">www.openweathermap.org</a>'s One Call API 3.0</div>
+                <div className='footerText'>Made using weather data from <a href='https://www.openweathermap.org' target="_blank"> One Call API 3.0</a> & geocoding with <a href='https://locationiq.com/' target='_blank'>LocationIQ</a></div>
             </div>
         </div>
     )
